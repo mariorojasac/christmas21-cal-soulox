c50c479c57f6bd6311dcd1c6b0d18f94e1b1cf41
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-11-28 14:51:12 -0500

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { createGlobalStyle } from "styled-components";
+import { StyledApp } from "./AppStyles";
+import { hatchArray } from "./helpers";
+import Hatch from "./Hatch";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: center / cover url("./images/background.jpg");
+    margin: 0;
+  }
+`;
 
 function App() {
+  const [hatches, setHatches] = useState([]);
+
+  useEffect(() => {
+    // Could use if statements instead off course
+    const calendar = localStorage.calendar
+      ? JSON.parse(localStorage.calendar)
+      : hatchArray();
+
+    setHatches(calendar);
+  }, []);
+
+  // Store calendar in localStorage
+  useEffect(() => {
+    // Could use if statements instead off course
+    hatches.length && localStorage.setItem("calendar", JSON.stringify(hatches));
+  }, [hatches]);
+
+  const handleFlipHatch = (id) => {
+    const updatedHatches = hatches.map((hatch) =>
+      hatch.id === id ? { ...hatch, open: !hatch.open } : hatch
+    );
+    setHatches(updatedHatches);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <div>
+          <h1>Merry Christmas 2021!</h1>
+        </div>
+        {hatches.map((hatch) => (
+          <Hatch
+            key={hatch.id}
+            hatchData={hatch}
+            handleClick={handleFlipHatch}
+          />
+        ))}
+      </StyledApp>
+    </>
   );
 }
 
---
+++ src/AppStyles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledApp = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  padding: 10%;
+  background-image: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/snow-bg.svg");
+  background-color: #89cff0;
+`;
